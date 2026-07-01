f26bc3165593375c7037556690a65b9415768e21
shabanvahid0101 <shabanvahid0101@gmail.com>
2026-07-01 13:07:52 +0900

    Make scheduled alerts more reliable

--- alert_bot.py
+++ alert_bot.py
@@ -142,7 +142,7 @@ def run_once(args: argparse.Namespace) -> dict:
     changed_signal = final["signal"] != last_signal or final["timestamp"] != last_timestamp
 
     should_send = args.telegram and is_actionable and (changed_signal or cooldown_passed)
-    if args.send_hold and args.telegram and changed_signal:
+    if args.send_hold and args.telegram:
         should_send = True
 
     if should_send:
